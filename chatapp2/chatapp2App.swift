649e743b840fab54706b949d384e5a7c4838fd10
tanmoy <roytanmoy07072001@gmail.com>
2024-03-10 11:41:38 +0530

    Initial Commit

---
+++ chatapp2/chatapp2App.swift
@@ -0,0 +1,17 @@
+//
+//  chatapp2App.swift
+//  chatapp2
+//
+//  Created by TANMOY ROY on 10/03/24.
+//
+
+import SwiftUI
+
+@main
+struct chatapp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ chatapp2UITests/chatapp2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  chatapp2UITestsLaunchTests.swift
+//  chatapp2UITests
+//
+//  Created by TANMOY ROY on 10/03/24.
+//
+
+import XCTest
+
+final class chatapp2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
